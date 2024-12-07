654faeaa853e1c41ea5a846632220da5c0708560
Alexander Morgunov <morgunov.02@list.ru>
2024-12-07 16:58:55 +0300

    Spring MVC + Hibernate

---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,31 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+    public void update(User user) {
+        entityManager.merge(user);
+    }
+    public void delete(Long id) {
+        entityManager.createQuery("delete from User where id=:id").setParameter("id", id).executeUpdate();
+    }
+    public User findById(Long id) {
+        return entityManager.find(User.class, id);
+    }
+    public List<User> findAll() {
+        return entityManager.createQuery("from User").getResultList();
+    }
+}
